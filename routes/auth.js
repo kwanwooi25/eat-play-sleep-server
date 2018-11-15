a81c7b95dad9e80f36da4a216de8c00ec930435a
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-11-15 11:13:45 +0900

    added console.log

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ const { onSuccess, onFail } = require('../utils/formatResponse');
 const redirectUser = (req, res) => res.redirect(`${process.env.HOST}`);
 
 const sendUserInfo = (req, res) => {
+  console.log(req.user);
   // if user logged in oauth
   if (req.user) res.json(onSuccess(req.user));
   // if user not logged in
