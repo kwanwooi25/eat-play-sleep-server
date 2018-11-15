0b4ea1cb2d02bf1ba7196e09e4afeeae26f1e260
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-11-16 08:58:55 +0900

    added cors headers

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ app.use(session({
 // Passport
 require('./services/passport')(app, passport);
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+  if ('OPTIONS' == req.method) res.send(200);
+  else next();
+});
+
 /** Routes */
 app.get('/', (req, res) => {
   res.send("This is EatPlaySleep Server");
