30aa71251d2e3d201ae2ae782fd3e03e21fda8d1
kwanwooi25 <kwanwoo.jeong@gmail.com>
2018-12-06 15:23:18 +0900

    Revert "changed activity between route options"
    This reverts commit 24e1582103642600f6acdcc1a9763257c35e5aef.

--- controllers/activity.js
+++ controllers/activity.js
@@ -29,11 +29,11 @@ const getActivitiesByBabyId = (req, res) => {
  */
 const getActivitiesBetween = (req, res) => {
   const { babyID, options } = req.query;
-  const { names, from, to } = JSON.parse(options);
+  const { name, from, to } = JSON.parse(options);
   
   db('activities')
     .where('baby_id', '=', babyID)
-    .andWhere(function() { this.whereIn('name', names) })
+    .andWhere('name', '=', name)
     .andWhereBetween('time_start', [from, to])
     .then(activities => res.json(onSuccess(activities)))
     .catch(error => res.json(onFail(error)));
